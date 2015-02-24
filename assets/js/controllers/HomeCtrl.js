4464a64c1ae907a0c18fa776e89a0750b13372cb
clairelyles <claire.lyles@gmail.com>
2015-02-24 01:03:53 -0800

    added s to http scripts on layout

--- assets/js/controllers/HomeCtrl.js
+++ assets/js/controllers/HomeCtrl.js
@@ -1,17 +1,17 @@
-// studmateApp.controller('HomeCtrl', ['$scope', '$http', '$modal', '$location', 'UserService', function($scope, $http, $modal, $location, UserService) {
+studmateApp.controller('HomeCtrl', ['$scope', '$http', '$modal', '$location', 'UserService', function($scope, $http, $modal, $location, UserService) {
 
 
-//   // initializes UserService
-//   $scope.UserService = UserService;
-//   $scope.$watchCollection('UserService', function() {
-//     $scope.currentUser = UserService.currentUser;
-//   });
+  // initializes UserService
+  $scope.UserService = UserService;
+  $scope.$watchCollection('UserService', function() {
+    $scope.currentUser = UserService.currentUser;
+  });
 
-//   $scope.showLogin = function() {
-//     $modal.open({
-//       templateUrl: '/views/authModal.html',
-//       controller: 'AuthModalCtrl'
-//     })
-//   };
+  $scope.showLogin = function() {
+    $modal.open({
+      templateUrl: '/views/authModal.html',
+      controller: 'AuthModalCtrl'
+    })
+  };
 
-// }]);
+}]);
